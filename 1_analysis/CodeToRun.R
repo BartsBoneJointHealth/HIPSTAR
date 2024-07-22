24d9471f89104029dc5802842e59d4d71c8e56e3
edward-burn <9583964+edward-burn@users.noreply.github.com>
2024-07-22 22:39:41 +0100

    single analysis code

--- 1_analysis/CodeToRun.R
+++ 1_analysis/CodeToRun.R
@@ -16,23 +16,23 @@ library(CohortConstructor)
 library(CohortCharacteristics)
 library(PatientProfiles)
 library(stringr)
+library(RPostgres)
+library(odbc)
 
-db <- DBI::dbConnect(RPostgres::Postgres(),
-                     dbname ="cdm_gold_202307",
-                     port = Sys.getenv("DB_PORT"),
-                     host = "163.1.65.51",
-                     user = Sys.getenv("DB_USER"),
-                     password =  Sys.getenv("DB_PASSWORD"))
+# Create the database connection
+db <- DBI::dbConnect("...")
 
-dbName <- "cprd 100k"
+# The name of the database
+dbName <- "..."
 
 # The name of the schema that contains the OMOP CDM with patient-level data
-cdmSchema <- "public_100k"
+cdmSchema <- "..."
 
 # The name of the schema where results tables will be created
-writeSchema <- "results"
+writeSchema <- "..."
 
-writePrefix <- "glp_"
+# A prefix to use when creating tables in the database
+writePrefix <- "hipstar"
 
 # minimum counts that can be displayed according to data governance
 minCellCount <- 5
@@ -41,7 +41,8 @@ minCellCount <- 5
 cdm <- cdmFromCon(
   con = db,
   cdmSchema = c(schema = cdmSchema),
-  writeSchema = c(schema = writeSchema, prefix = writePrefix),
+  writeSchema = c(schema = writeSchema,
+                  prefix = writePrefix),
   cdmName = dbName,
   .softValidation = TRUE
 )
